a41c7df90db65f654abab596c4d3bd49bbe34acf
TetsuyaNegishi <yagamiraito1009@gmail.com>
2014-10-23 20:54:06 +0900

    ビリヤード盤、クッション描画

--- Billiards/Game.cpp
+++ Billiards/Game.cpp
@@ -8,10 +8,26 @@ void Game::init(){
 	ChangeWindowMode(TRUE), DxLib_Init(), SetDrawScreen(DX_SCREEN_BACK); //ウィンドウモード変更と初期化と裏画面設定
 }
 
+void Game::boardShow(){
+	const int BROWN = GetColor(153, 76, 0);
+	const int DARK_GREEN = GetColor(0, 100, 0);
+	const int GREEN = GetColor(0, 200, 0);
+
+	//ビリヤード盤描画
+	DrawBox(BOARD_LEFT, BOARD_TOP, BOARD_RIGHT, BOARD_BOTTOM, BROWN, TRUE);
+	DrawBox(FIELD_LEFT, FIELD_TOP, FIELD_RIGHT, FIELD_BOTTOM, DARK_GREEN, TRUE);
+	//クッション描画
+	DrawBox(FIELD_LEFT, FIELD_TOP, CUSHION_LEFT, FIELD_BOTTOM, GREEN, TRUE);
+	DrawBox(FIELD_LEFT, FIELD_TOP, CUSHION_LEFT, FIELD_BOTTOM, GREEN, TRUE);
+	DrawBox(FIELD_LEFT, FIELD_TOP, CUSHION_LEFT, FIELD_BOTTOM, GREEN, TRUE);
+	DrawBox(FIELD_LEFT, FIELD_TOP, CUSHION_LEFT, FIELD_BOTTOM, GREEN, TRUE);
+
+}
+
 void Game::main(){
 	// while(裏画面を表画面に反映, メッセージ処理, 画面クリア)
 	while (ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0){
-
+		boardShow();
 	}
 }
 
